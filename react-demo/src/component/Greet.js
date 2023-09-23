87a6c5326e6bb0bd67d746a1709db17e5e869500
JoyMatubber <joy.matubber.41959@gmail.com>
2023-09-23 21:10:32 +0600

    Added name export and default component export

---
+++ react-demo/src/component/Greet.js
@@ -0,0 +1,9 @@
+export const Name = () => {
+  // use export so use same name (Name) as a component
+  return <h1>Hi Joy</h1>;
+};
+const age = () => {
+  //  use default   export so use any name as  a component
+  return <h1>My age is 23</h1>;
+};
+export default age;
