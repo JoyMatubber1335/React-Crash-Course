b19d4222bc2b6ac79f9ee76df4e19cceb75e3b45
JoyMatubber <joy.matubber.41959@gmail.com>
2023-09-24 23:40:52 +0600

    Added basic memo function

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 // // import { Name } from "./component/Greet"; // I write {Name } like this because we pass the Component
 // import Myage from "./component/Greet"; // I write Myage like this because we don't pass the default Component
 // import { Msg } from "./component/State";
@@ -9,7 +10,18 @@ import "./App.css";
 // import { UseState } from "./component/UseState";
 // import { UseEffect } from "./component/UseEffect";
 import { FetchData } from "./component/FetchData";
+import { MemoFunction } from "./component/MemoFunction";
 function App() {
+  const [c, cc] = useState(0);
+  const [c1, cc1] = useState(0);
+  console.log("app render ");
+  const ccc = () => {
+    cc((c) => c + 1);
+  };
+  const demo = () => {
+    cc1((c1) => c1 + 1);
+  };
+
   return (
     <div className="App">
       {/* {/* <Name id="23" />
@@ -25,7 +37,15 @@ function App() {
       <ListRender /> */}
       {/* <UseState /> */}
       {/* <UseEffect /> */}
-      <FetchData />
+      <h1>{c}</h1>
+      <h1>{c1}</h1>
+
+      <button onClick={ccc}>click</button>
+      {/* render the memo function */}
+      <button onClick={demo}>click1</button>
+      {/* not render the memo Function  */}
+      {/* <FetchData /> */}
+      <MemoFunction count={c} />
     </div>
   );
 }
