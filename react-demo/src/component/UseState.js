57654a3c8261246eaf87e64cc4842535c2f98883
JoyMatubber <joy.matubber.41959@gmail.com>
2023-09-24 20:01:12 +0600

    Added multiple useState hook

--- react-demo/src/component/UseState.js
+++ react-demo/src/component/UseState.js
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 export const UseState = () => {
   const [count, serCount] = useState(4);
+  const [color, setColor] = useState("blue");
   const decriment = () => {
     serCount((previous) => previous - 1);
+    setColor("red");
   };
   const incriment = () => {
     serCount((previous) => previous + 1);
+    setColor("green");
   };
   return (
     <div>
       <button onClick={decriment}>-</button>
       <span>{count}</span>
+      <span>{color}</span>
       <button onClick={incriment}>+</button>
     </div>
   );
