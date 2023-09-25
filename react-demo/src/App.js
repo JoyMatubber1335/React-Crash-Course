554c8ef29b7bf92c95e2c38d69203d6cce15177e
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2023-09-25 12:32:55 +0600

    Added use call back

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-// import { useState } from "react";
+import { useCallback, useState } from "react";
 // // import { Name } from "./component/Greet"; // I write {Name } like this because we pass the Component
 // import Myage from "./component/Greet"; // I write Myage like this because we don't pass the default Component
 // import { Msg } from "./component/State";
@@ -10,22 +10,30 @@ import "./App.css";
 // import { UseState } from "./component/UseState";
 // import { UseEffect } from "./component/UseEffect";
 // import { FetchData } from "./component/FetchData";
-// import { MemoFunction } from "./component/MemoFunction";
-import { UseMemo } from "./component/UseMemo";
+import { MemoFunction } from "./component/MemoFunction";
+// import { UseMemo } from "./component/UseMemo";
 
 function App() {
-  // const [c, cc] = useState(0);
-  // const [c1, cc1] = useState(0);
-  // console.log("app render ");
-  // const ccc = () => {
-  //   cc((c) => c + 1);
-  // };
-  // const demo = () => {
-  //   cc1((c1) => c1 + 1);
-  // };
+  const [count, setCount] = useState(0);
+  const [togle, setTogle] = useState(0);
+  console.log("app render ");
+  const handelClick = () => {
+    setCount((count) => count + 1);
+  };
+  const handelIncriment = useCallback(() => {
+    return setCount(count + 1);
+  }, [count]);
 
   return (
     <div className="App">
+      {togle ? "on" : "off"}
+      <button
+        onClick={() => {
+          setTogle(!togle);
+        }}
+      >
+        toggle
+      </button>
       {/* {/* <Name id="23" />
       <Name id="23" />
       <Name id="23" /> }
@@ -39,16 +47,13 @@ function App() {
       <ListRender /> */}
       {/* <UseState /> */}
       {/* <UseEffect /> */}
-      {/* <h1>{c}</h1>
-      <h1>{c1}</h1> */}
-
-      {/* <button onClick={ccc}>click</button> */}
-      {/* render the memo function */}
+      {/* <h1>{count}</h1> */}
+      <button onClick={handelClick}>click</button> {/* render the memo function */}
       {/* <button onClick={demo}>click1</button> */}
       {/* not render the memo Function  */}
       {/* <FetchData /> */}
-      {/* <MemoFunction count={c} /> */}
-      <UseMemo />
+      <MemoFunction count={count} onHandelIncrement={handelIncriment} />
+      {/* <UseMemo /> */}
     </div>
   );
 }
--- react-demo/src/component/MemoFunction.js
+++ react-demo/src/component/MemoFunction.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { memo } from "react";
-export const MemoFunction = memo(({ count }) => {
+export const MemoFunction = memo(({ count, onHandelIncrement }) => {
   // use memeo so that can't render
   console.log("memo render ");
-  return <div>MemoFunction</div>;
+  return (
+    <div>
+      MemoFunction {count}
+      <button onClick={onHandelIncrement}>Increment count</button>
+    </div>
+  );
 });
 
 // or
