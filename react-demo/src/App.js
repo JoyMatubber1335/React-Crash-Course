df5c05a0ffa61a5e771132bd1dd464c2f3dc878f
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2023-09-25 11:09:35 +0600

    Added useMemo hook

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+// import { useState } from "react";
 // // import { Name } from "./component/Greet"; // I write {Name } like this because we pass the Component
 // import Myage from "./component/Greet"; // I write Myage like this because we don't pass the default Component
 // import { Msg } from "./component/State";
@@ -9,18 +9,20 @@ import { useState } from "react";
 // import { ListRender } from "./component/ListRender";
 // import { UseState } from "./component/UseState";
 // import { UseEffect } from "./component/UseEffect";
-import { FetchData } from "./component/FetchData";
-import { MemoFunction } from "./component/MemoFunction";
+// import { FetchData } from "./component/FetchData";
+// import { MemoFunction } from "./component/MemoFunction";
+import { UseMemo } from "./component/UseMemo";
+
 function App() {
-  const [c, cc] = useState(0);
-  const [c1, cc1] = useState(0);
-  console.log("app render ");
-  const ccc = () => {
-    cc((c) => c + 1);
-  };
-  const demo = () => {
-    cc1((c1) => c1 + 1);
-  };
+  // const [c, cc] = useState(0);
+  // const [c1, cc1] = useState(0);
+  // console.log("app render ");
+  // const ccc = () => {
+  //   cc((c) => c + 1);
+  // };
+  // const demo = () => {
+  //   cc1((c1) => c1 + 1);
+  // };
 
   return (
     <div className="App">
@@ -37,15 +39,16 @@ function App() {
       <ListRender /> */}
       {/* <UseState /> */}
       {/* <UseEffect /> */}
-      <h1>{c}</h1>
-      <h1>{c1}</h1>
+      {/* <h1>{c}</h1>
+      <h1>{c1}</h1> */}
 
-      <button onClick={ccc}>click</button>
+      {/* <button onClick={ccc}>click</button> */}
       {/* render the memo function */}
-      <button onClick={demo}>click1</button>
+      {/* <button onClick={demo}>click1</button> */}
       {/* not render the memo Function  */}
       {/* <FetchData /> */}
-      <MemoFunction count={c} />
+      {/* <MemoFunction count={c} /> */}
+      <UseMemo />
     </div>
   );
 }
